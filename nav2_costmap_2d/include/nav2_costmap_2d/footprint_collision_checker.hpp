c073149a752d013224949f042db911487654e97f
Tony Najjar <tony.najjar.1997@gmail.com>
2025-06-10 22:55:32 +0200

    implement footprintAreaCost
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>

--- nav2_costmap_2d/include/nav2_costmap_2d/footprint_collision_checker.hpp
+++ nav2_costmap_2d/include/nav2_costmap_2d/footprint_collision_checker.hpp
@@ -21,6 +21,7 @@
 #include <vector>
 #include <memory>
 #include <algorithm>
+#include <limits>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
@@ -53,6 +54,10 @@ public:
    */
   double footprintCost(const Footprint & footprint);
   /**
+   * @brief Find the footprint cost for entire footprint area using full coverage
+   */
+  double footprintAreaCost(const Footprint & footprint);
+  /**
    * @brief Find the footprint cost a a post with an unoriented footprint
    */
   double footprintCostAtPose(double x, double y, double theta, const Footprint & footprint);
@@ -81,6 +86,17 @@ public:
   }
 
 protected:
+  /**
+   * @brief Check if a point is inside the footprint polygon
+   * @param x X coordinate in world frame
+   * @param y Y coordinate in world frame  
+   * @param footprint Footprint polygon
+   * @return True if point is inside footprint
+   */
+  bool isPointInFootprint(
+    double x, double y,
+    const Footprint & footprint);
+
   CostmapT costmap_;
 };
 
