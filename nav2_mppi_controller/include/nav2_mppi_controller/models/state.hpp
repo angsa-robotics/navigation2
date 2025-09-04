d66d68af59fcfb53b0cf19bbb8a58aa29c84a35f
Tony Najjar <tony.najjar.1997@gmail.com>
2025-09-04 21:11:58 +0200

    Use integral path distance for MPPI Critics  (#5495)
    * Implement integral distance for critics
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * .
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * linting
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * more linting
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix tests
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * reset path length
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix sign
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix return value
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * Revert "fix return value"
    
    This reverts commit 8b21e890de598ab6b35c4a0200c82ebfc4b0c9c8.
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * remove extra variable
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * add doxygen
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * rename getCriticGoalPathDistance
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * delete withinPositionGoalTolerance
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * use only one path distance
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * cleanup
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * linting
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * reorder
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * format
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    * fix tests
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>
    
    ---------
    
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>

--- nav2_mppi_controller/include/nav2_mppi_controller/models/state.hpp
+++ nav2_mppi_controller/include/nav2_mppi_controller/models/state.hpp
@@ -41,6 +41,7 @@ struct State
   geometry_msgs::msg::PoseStamped pose;
   float local_path_length;
   geometry_msgs::msg::Twist speed;
+  float local_path_length;
 
   /**
     * @brief Reset state data
